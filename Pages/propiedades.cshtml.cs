91166e4852004e96781b89b4b3bc41c7eff806cf
Tirofijo26 <jhon92600@gmail.com>
2023-11-20 15:12:33 -0500

    cambios ulloa

---
+++ Pages/propiedades.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace vendedor.Pages
+{
+    public class propiedadesModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}

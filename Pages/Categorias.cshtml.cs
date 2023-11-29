1f5c37b572c80cae9458959b69cb32b3112675bd
AdrianZarate <adrian.1478.rz@gmail.com>
2023-11-29 01:33:27 -0500

    merge

---
+++ Pages/Categorias.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace vendedor.Pages;
+
+public class CategoriasModel : PageModel
+{
+    public void OnGet()
+    {
+    }
+}
+
